0f64cd58a9774765c87ff4d68a7116d540e1834b
Admin <hiuteddy204@gmail.com>
2023-11-02 11:10:36 +0700

    Initial commit

---
+++ app/src/main/java/hieunnph32561/fpoly/du_an_1/MainActivity.java
@@ -0,0 +1,14 @@
+package hieunnph32561.fpoly.du_an_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
